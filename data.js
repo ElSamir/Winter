db6b5d67db903daac31f14c1fce080b40415a159
Samir <89761444+ElSamir@users.noreply.github.com>
2024-07-06 23:58:33 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let songs = [
+    {
+        name: 'Sour grapes',
+        path: 'assets/musics/sour_grapes.mp3',
+        artist: 'Kim Jisso',
+        cover: 'assets/images/Sour_grapes.jpeg'
+    },
+
+{
+    name: 'You bellong with me',
+        path: 'assets/musics/you_belong_with_me.mp3',
+        artist: 'Kim Jisso',
+        cover: 'assets/images/You_belong_with_me.jpeg'
+},
+
+{
+    name: 'All to well',
+        path: 'assets/musics/all_to_whell.mp3',
+        artist: 'Kim Jisso',
+        cover: 'assets/images/All_to_Whell.jpeg'
+},
+{
+    name: 'Until I found you',
+    path: 'assets/musics/until_i_found_you.mp3',
+    artist: 'Kim Jisso',
+    cover: 'assets/images/Until_i_found_her.jpeg'
+}
+]
